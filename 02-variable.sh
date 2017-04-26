1146d5838581912395e53f57c59df7f71631a2f0
Nephtalie Barthelemy <b.nephtalie@hotmail.com>
2017-04-26 15:51:50 -0400

    Le nom de ce fichier était à l'origine 02-variables.sh mais il y a eu une faute de frappe alors il est devenu le fichier de référence

---
+++ 02-variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ne pas mettre d'espace 
+
+prenom-John
+
+echo $prenom
+
+declare -i age=35 # un int 
+declare -r age2=33  # Devient en lecture seule 
+
+#On met des guillemets lorsqu'il y a des espaces 
+cours="Cours de Linux"
+
+echo Le programme $0 a pris le paramètre $1
